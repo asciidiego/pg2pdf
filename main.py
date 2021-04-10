f307d86575d5b7a9d7246e5845017f10e26a33c8
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-04-11 01:03:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import urllib.request
+import html_text
+
+essay_canonical_name = input(
+    """
+Please, enter the canonical name of Paul Graham essay.
+    
+For example, for extracting the text of "What You Wish You'd Known"
+(http://www.paulgraham.com/hs.html), you'd input "hs".
+
+>> """
+)
+
+essay_url = f"http://www.paulgraham.com/{essay}.html"
+
+response = urllib.request.urlopen(essay_url)
+response_as_html = response.read().decode("utf-8")
+parsed_text = html_text.extract_text(response_as_html)
+
+print(parsed_text)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# pg2pdf
+
+a script to turn a paul graham essay into a printable PDF
+
+##
